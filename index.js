e29b6076ccf4f0446700778b2800e6fc82a0a8f0
Nick Inman <ninman@Nicks-MBP.fios-router.home>
2019-03-18 13:26:52 -0700

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,27 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation() {
+  return document.querySelector('input').value
+}
+
+function addNewElementAsLi() {
+  let node = document.createElement('li')
+  let textnode = document.createTextNode(retrieveEmployeeInformation())
+  node.appendChild(textnode)
+  document.querySelector('.employee-list').appendChild(node.appendChild(textnode))
+}
+
+function addNewLiOnClick() {
+  document.querySelector('form').addEventListener('click', function(event){
+    addNewElementAsLi()
+    document.querySelector('input').value = ''
+  })
+}
+
+function clearEmployeeListOnLinkClick() {
+  document.querySelector('a').addEventListener('click', function(event){
+    document.querySelector('.employee-list').textContent = ''
+  })
+}
