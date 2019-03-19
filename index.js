d58be350ba72d668b895dbdeb1e2d3a9af65f823
Elise Phan <aliceconnic@gmail.com>
2019-03-19 01:31:52 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,31 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+let input = document.querySelector('input');
+
+function retrieveEmployeeInformation() {
+ return input.value;
+}
+
+function addNewElementAsLi() {
+  let newEmployee = retrieveEmployeeInformation();
+  return document.querySelector('ul.employee-list').insertAdjacentHTML(
+    'beforeend', `<li>${newEmployee}`);
+}
+
+function addNewLiOnClick() {
+  let submit = document.querySelector('input[type="submit"]');
+  submit.addEventListener('click', function(event){
+    addNewElementAsLi();
+    input.value = '';
+  });
+}
+
+function clearEmployeeListOnLinkClick() {
+  let clear = document.querySelector("a[href='#']");
+  let list = document.querySelector('ul');
+  clear.addEventListener('click', function(event){
+    list.innerHTML = ''
+  });
+}
